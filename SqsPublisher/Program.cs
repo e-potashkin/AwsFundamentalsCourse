35965e062ec251473fc748cd657a7c35b6d9f213
Eugene Potashkin <yevhenii.potashkin@arvosoftware.com>
2023-01-22 20:40:21 +0200

    iniy

---
+++ SqsPublisher/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.Json;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using SqsPublisher;
+
+var sqsClient = new AmazonSQSClient();
+
+var customer = new CustomerCreated
+{
+    Id = Guid.NewGuid(),
+    Email = "eugene@potashkin.com",
+    FullName = "Eugene Potashkin",
+    DateOfBirth = new DateTime(1992, 4, 27),
+    GitHubUsername = "e-potashkin"
+};
+
+var queueUriResponse = await sqsClient.GetQueueUrlAsync("customers");
+
+var sendMessageRequest = new SendMessageRequest
+{
+    QueueUrl = queueUriResponse.QueueUrl,
+    MessageBody = JsonSerializer.Serialize(customer),
+    MessageAttributes = new Dictionary<string, MessageAttributeValue>
+    {
+        {
+            "MessageType", new MessageAttributeValue
+            {
+                DataType = "String",
+                StringValue = nameof(CustomerCreated)
+            }
+        }
+    }
+};
+
+var response = await sqsClient.SendMessageAsync(sendMessageRequest);
+Console.WriteLine();
